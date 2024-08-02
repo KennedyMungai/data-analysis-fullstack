78647ac82ec8fcf4e041f22a20fd6a253a9825d0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-02 14:11:50 +0300

    Made some auth changes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { ReactNode } from 'react'
 
 type Props = {
@@ -5,7 +6,17 @@ type Props = {
 }
 
 const AuthLayout = ({ children }: Props) => {
-	return <div>{children}</div>
+	return (
+		<div className='flex h-full'>
+			<div className='bg-slate-500 flex flex-col items-center justify-center flex-1'>
+				<Image src='/logo.svg' alt='logo' width={200} height={200} />
+				<span className='text-3xl font-semibold'>Data Analysis</span>
+			</div>
+			<div className=' flex items-center justify-center flex-1 bg-white'>
+				{children}
+			</div>
+		</div>
+	)
 }
 
 export default AuthLayout
