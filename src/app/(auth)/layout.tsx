8ced395ca1216e81505e2b20ccb9b99816fcab05
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-09 14:37:41 +0300

    Added a simple spin to the logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,7 +9,13 @@ const AuthLayout = ({ children }: Props) => {
 	return (
 		<div className='flex h-full'>
 			<div className='bg-slate-500 flex flex-col items-center justify-center flex-1'>
-				<Image src='/logo.svg' alt='logo' width={200} height={200} />
+				<Image
+					src='/logo.svg'
+					alt='logo'
+					width={200}
+					height={200}
+					className='animate-spin duration-200'
+				/>
 				<span className='text-3xl font-semibold'>Data Analysis</span>
 			</div>
 			<div className=' flex items-center justify-center flex-1 bg-white'>
